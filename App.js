d447ae534e142dfddb1964e37835a002c0f3c64b
Shakthi priyanka <128129803+Shakthipriyaanka@users.noreply.github.com>
2023-03-29 11:09:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import World from './World';
+
+function App() {
+  return (
+    <div className="App">
+<World/>
+    </div>
+  );
+}
+
+export default App;
---
+++ World.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function World() {
+  return (
+    <div>
+      <p>This is World Functional Component </p>
+    </div>
+  )
+}
+
+export default World
